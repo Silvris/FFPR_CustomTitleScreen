c266298866a8d3b59ae68b28dfd9eb051337fb53
Silvris <58583688+Silvris@users.noreply.github.com>
2021-09-30 13:48:16 -0500

    initial commit

---
+++ FFPR_CustomTitleScreen/EntryPoint.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+
+namespace FFPR_CustomTitleScreen
+{
+    [BepInPlugin("silvris.ffpr.custom_title_screen", "Custom Title Screen", "1.0.0.0")]
+    [BepInProcess("FINAL FANTASY.exe")]
+    [BepInProcess("FINAL FANTASY II.exe")]
+    [BepInProcess("FINAL FANTASY III.exe")]
+    [BepInProcess("FINAL FANTASY IV.exe")]
+    [BepInProcess("FINAL FANTASY V.exe")]
+    [BepInProcess("FINAL FANTASY VI.exe")]
+    public class EntryPoint : BasePlugin
+    {
+        public override void Load()
+        {
+            Log.LogInfo("Loading...");
+            ClassInjector.RegisterTypeInIl2Cpp<ModComponent>();
+            String name = typeof(ModComponent).FullName;
+            Log.LogInfo($"Initializing in-game singleton: {name}");
+            GameObject singleton = new GameObject(name);
+            singleton.hideFlags = HideFlags.HideAndDontSave;
+            GameObject.DontDestroyOnLoad(singleton);
+            Log.LogInfo("Adding ModComponent to singleton...");
+            ModComponent component = singleton.AddComponent<ModComponent>();
+            if (component is null)
+            {
+                GameObject.Destroy(singleton);
+                throw new Exception($"The object is missing the required component: {name}");
+            }
+        }
+    }
+}
---
+++ FFPR_CustomTitleScreen/ModComponent.cs
@@ -0,0 +1,105 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace FFPR_CustomTitleScreen
+{
+    class ModComponent : MonoBehaviour
+    {
+        public static ModComponent Instance { get; private set; }
+        public static ManualLogSource Log { get; private set; }
+        private Boolean _isDisabled = false;
+        private String _filePath;
+        public ModComponent(IntPtr ptr) : base(ptr)
+        {
+        }
+        public void Awake()
+        {
+            Assembly thisone = Assembly.GetExecutingAssembly();
+            _filePath = Path.GetDirectoryName(thisone.Location);
+            Log = BepInEx.Logging.Logger.CreateLogSource("FFPR_CustomTitleScreen");
+            try
+            {
+                Instance = this;
+                Log.LogMessage($"[{nameof(ModComponent)}].{nameof(Awake)}: Processed successfully.");
+            }
+            catch (Exception ex)
+            {
+                _isDisabled = true;
+                Log.LogError($"[{nameof(ModComponent)}].{nameof(Awake)}(): {ex}");
+                throw;
+            }
+
+        }
+        bool isScene_CurrentlyLoaded(string sceneName_no_extention)
+        {
+            for (int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if (scene.name == sceneName_no_extention)
+                {
+                    //the scene is already loaded
+                    return true;
+                }
+            }
+
+            return false;//scene not currently loaded in the hierarchy
+        }
+        public static Texture2D ReadTextureFromFile(String fullPath, String Name)
+        {
+            try
+            {
+                Byte[] bytes = File.ReadAllBytes(fullPath);
+                Texture2D texture = new Texture2D(1, 1, TextureFormat.ARGB32, false) { name = Name };
+                texture.filterMode = FilterMode.Point;
+                if (!ImageConversion.LoadImage(texture, bytes))
+                    throw new NotSupportedException($"Failed to load texture from file [{fullPath}]");
+
+                return texture;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        public void Update()
+        {
+            try
+            {
+                //Log.LogInfo("ModComponent.Update()");
+                if (_isDisabled)
+                {
+                    return;
+                }
+                if (isScene_CurrentlyLoaded("TitleScreen"))
+                {
+                    GameObject background = GameObject.Find("background_canvas/ui_root/backgrou_root/background");
+                    if(background != null)
+                    {
+                        Image image = background.GetComponent<Image>();
+                        Texture2D tex = ReadTextureFromFile(_filePath + "/customTitleScreen.png", "customTitleScreen");
+                        image.sprite = Sprite.Create(tex, new Rect(0,0,tex.width,tex.height), new Vector2(), 1f);
+                        _isDisabled = true;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                _isDisabled = true;
+                Log.LogError($"[{nameof(ModComponent)}].{nameof(Update)}(): {ex}");
+                throw;
+            }
+
+        }
+    }
+}
